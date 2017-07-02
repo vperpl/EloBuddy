0c2686c9ea729491f143df797dd0402ed48eca9a
Xan Kriegor <sniper_evli@o2.pl>
2017-07-03 00:54:26 +0200

    Update SpellsManager.cs

--- Yasuo OP/Yasuo OP/SpellsManager.cs
+++ Yasuo OP/Yasuo OP/SpellsManager.cs
@@ -114,7 +114,7 @@ namespace Yasuo_OP
                 case SpellSlot.E:
                     var bonusDamage = new[] {15.0f, 17f, 20.5f, 22.5f, 25.5f}[level]*ECount();
 
-                    dmg += new[] {60f, 70f, 80f, 90f, 100f}[level] + Me.FlatMagicDamageMod*0.6f + bonusDamage;
+                    dmg += new[] {60f, 68f, 80f, 90f, 100f}[level] + Me.FlatMagicDamageMod*0.6f + bonusDamage;
                     break;
                 case SpellSlot.R:
                     dmg += new[] {200f, 300f, 400f}[level] + Me.FlatPhysicalDamageMod*1.5f;
